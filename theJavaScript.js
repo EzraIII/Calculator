f48f292097e7679db5711ef7da128bafeba54b92
EzraIII <Ezra3Ezra3@gmail.com>
2019-04-04 23:22:26 -0700

    Changed css to be better and everything else was changed a little bit in the process.

--- theJavaScript.js
+++ theJavaScript.js
@@ -2,63 +2,64 @@ let firstNumber = '0';
 let operation = '';
 let secondNumber = '';
 let hasDecimalPoint = false;
-function autoFix() {
-    if(operation!='') {
-        document.getElementById("myText").innerHTML = firstNumber + ' ' + operation + ' ' + secondNumber;
+let activeNumber = firstNumber;
+
+function autoUpdate() {
+    if(operation=='') {
+        firstNumber = activeNumber;
     } else {
-        document.getElementById("myText").innerHTML = firstNumber;
+        secondNumber = activeNumber;
     }
 }
+
+function updateDisplay() {
+    document.getElementById("myText").innerHTML = (firstNumber + ' ' + operation + ' ' + secondNumber).trim();
+}
+
 function calculate() {
     firstNumber = String(eval(firstNumber + operation + secondNumber));
     operation = '';
     secondNumber = '';
-    hasDecimalPoint = false;
-    autoFix();
+    hasDecimalPoint = firstNumber.includes('.');
+    activeNumber = firstNumber
+    updateDisplay();
 }
+
 function clearString() {
     firstNumber = '0';
     operation = '';
     secondNumber = '';
     hasDecimalPoint = false;
-    autoFix();
+    activeNumber = firstNumber;
+    updateDisplay();
 }
-function myFunction(character, isOperation) {
-    if(operation!='') {
-        if(isOperation) {
-            calculate();
-            operation = character;
-            secondNumber = '0';
-            hasDecimalPoint = false;
-        } else if(character=='.') {
-            if(hasDecimalPoint==false) {
-                secondNumber = secondNumber + character;
-                hasDecimalPoint = true;
-            }
-        } else {
-            if(secondNumber=='0') {
-                secondNumber = character;
-            } else {
-                secondNumber = secondNumber + character;
-            }
-        }
+
+function addNumber(character) {
+    if(activeNumber=='0') {
+        activeNumber = character;
     } else {
-        if(isOperation) {
-            operation = character;
-            secondNumber = '0';
-            hasDecimalPoint = false;
-        } else if(character=='.') {
-            if(hasDecimalPoint==false) {
-                firstNumber = firstNumber + character;
-                hasDecimalPoint = true;
-            }
-        } else {
-            if(firstNumber=='0') {
-                firstNumber = character;
-            } else {
-                firstNumber = firstNumber + character;
-            }
-        }
+        activeNumber += character;
     }
-    autoFix();
+    autoUpdate();
+    updateDisplay();
+}
+
+function addOperation(character) {
+    if(operation!='') {
+        calculate();
+    }
+    operation = character;
+    secondNumber = '0';
+    hasDecimalPoint = false;
+    activeNumber = '0';
+    updateDisplay();
+}
+
+function addDecimalPoint() {
+    if(hasDecimalPoint==false) {
+        activeNumber += '.';
+        hasDecimalPoint = true;
+        autoUpdate();
+        updateDisplay();
+    }
 }
