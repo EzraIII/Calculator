7b889f33f89250cc39f92b8fd7180d9dd69255b8
EzraIII <Ezra3Ezra3@gmail.com>
2019-04-20 14:01:34 -0700

    added PNGs inserted them and changed a lot of other thing

--- theJavaScript.js
+++ theJavaScript.js
@@ -1,43 +1,63 @@
+"use strict";
+
 let firstNumber = '0';
 let operation = '';
 let secondNumber = '';
 let hasDecimalPoint = false;
 let activeNumber = firstNumber;
+let numberOfCharacters = firstNumber.length + operation.length + secondNumber.length;
 
+function checkIfValid() { // incorrectly named
+    if (numberOfCharacters >= 17 || firstNumber.length >= 15) { // Redundant
+        return false;
+    } else {
+        return true;
+    }
+}
 function autoUpdate() {
-    if(operation=='') {
+    if(operation=='') { // === instead of ==
         firstNumber = activeNumber;
     } else {
         secondNumber = activeNumber;
     }
+    numberOfCharacters = firstNumber.length + operation.length + secondNumber.length;
 }
-
 function updateDisplay() {
-    document.getElementById("myText").innerHTML = (firstNumber + ' ' + operation + ' ' + secondNumber).trim();
+    if(operation==='') {
+        document.getElementById('display').innerHTML = firstNumber; // Cache retrieved DOM elements. DOM queries are expensive
+    } else {
+        document.getElementById('display').innerHTML = firstNumber + ' ' + operation + ' ' + secondNumber;
+    }
 }
 
 function calculate() {
-    firstNumber = String(eval(firstNumber + operation + secondNumber));
+    firstNumber = String(eval(firstNumber + operation + secondNumber)); // eval is dangerous, generally
     operation = '';
     secondNumber = '';
+    if (firstNumber.length > 15) {
+        firstNumber = String(Number(firstNumber).toPrecision(10)); // Avoid setting a value twice, if once would suffice
+    }
     hasDecimalPoint = firstNumber.includes('.');
     activeNumber = firstNumber
     updateDisplay();
 }
 
 function clearString() {
+    // Is setting duplicate values as the top of the file.
     firstNumber = '0';
     operation = '';
     secondNumber = '';
     hasDecimalPoint = false;
     activeNumber = firstNumber;
+    // Doesn't update numberOfCharacters    
     updateDisplay();
 }
 
 function addNumber(character) {
-    if(activeNumber=='0') {
-        activeNumber = character;
-    } else {
+    if(activeNumber=='0') { // === instead of ==
+        activeNumber = '';
+    }
+    if(checkIfValid()) {
         activeNumber += character;
     }
     autoUpdate();
@@ -45,7 +65,7 @@ function addNumber(character) {
 }
 
 function addOperation(character) {
-    if(operation!='') {
+    if(operation!='') { // !== instead of !=
         calculate();
     }
     operation = character;
@@ -56,10 +76,12 @@ function addOperation(character) {
 }
 
 function addDecimalPoint() {
-    if(hasDecimalPoint==false) {
-        activeNumber += '.';
-        hasDecimalPoint = true;
-        autoUpdate();
-        updateDisplay();
+    if (checkIfValid()) {
+        if(hasDecimalPoint==false) {
+            activeNumber += '.';
+            hasDecimalPoint = true;
+            autoUpdate();
+            updateDisplay();
+        }
     }
 }
